36996486f22341cd1a7ce7da562392ac4e835303
danielnorberg1 <danno@student.chalmers.se>
2025-03-18 23:20:51 +0100

    Add the board to be solved

--- source/main.c
+++ source/main.c
@@ -1,11 +1,41 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
+#include <stdbool.h>
 
-
-
+#define SIZE 9
 
 
 
 int main(){
 
+srand(time(NULL));
+
+// Here we create the sudoku
+// Right now we create it manually so take one from the internet
+// or make it yourself
+int board[SIZE][SIZE]= {
+    {5, 3, 0,   0, 7, 0,   0, 0, 0},
+    {6, 0, 0,   1, 9, 5,   0, 0, 0},
+    {0, 9, 8,   0, 0, 0,   0, 6, 0},
+
+    {8, 0, 0,   0, 6, 0,   0, 0, 3},
+    {4, 0, 0,   8, 0, 3,   0, 0, 1},
+    {7, 0, 0,   0, 2, 0,   0, 0, 6},
+    
+    {0, 6, 0,   0, 0, 0,   2, 8, 0},
+    {0, 0, 0,   4, 1, 9,   0, 0, 5},
+    {0, 0, 0,   0, 8, 0,   0, 7, 9}
 }
+
+// Press 1 to solve it yourself
+
+
+// Press 2 let the robot solve it.
+
+
+
+
+}
+
+
